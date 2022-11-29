ec11b415b5e22c3be5c1e714df438fcc0823fd5b
tpkeeper <tpkeeper.me@gmail.com>
2022-11-29 15:25:38 +0800

    support target chainId and stakepool

--- contracts/stake_portal/StakePortal.sol
+++ contracts/stake_portal/StakePortal.sol
@@ -6,26 +6,62 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract StakePortal is Ownable {
     using SafeERC20 for IERC20;
-    using SafeMath for uint256;
 
     // Events
-    event Stake(address staker, uint256 amount, bytes data);
+    event Stake(
+        address staker,
+        address stakePool,
+        uint256 amount,
+        bytes recipient,
+        uint8 chainId
+    );
 
     address public erc20TokenAddress;
-    address public stakePoolAddress;
     uint256 public minAmount;
     uint256 public relayFee;
+    mapping(address => bool) public stakePoolAddressExist;
+    mapping(uint8 => bool) public chainIdExist;
 
     constructor(
+        address[] memory _stakePoolAddressList,
+        uint8[] memory _destChainIdList,
         address _erc20TokenAddress,
-        address _stakePoolAddress,
         uint256 _minAmount,
         uint256 _relayFee
     ) {
+        for (uint256 i = 0; i < _stakePoolAddressList.length; i++) {
+            stakePoolAddressExist[_stakePoolAddressList[i]] = true;
+        }
+
+        for (uint256 i = 0; i < _destChainIdList.length; i++) {
+            chainIdExist[_destChainIdList[i]] = true;
+        }
+
         erc20TokenAddress = _erc20TokenAddress;
-        stakePoolAddress = _stakePoolAddress;
         minAmount = _minAmount;
         relayFee = _relayFee;
+    }
+
+    function addStakePool(
+        address[] memory _stakePoolAddressList
+    ) external onlyOwner {
+        for (uint256 i = 0; i < _stakePoolAddressList.length; i++) {
+            stakePoolAddressExist[_stakePoolAddressList[i]] = true;
+        }
+    }
+
+    function rmStakePool(address _stakePoolAddress) external onlyOwner {
+        delete stakePoolAddressExist[_stakePoolAddress];
+    }
+
+    function addChainId(uint8[] memory _chaindIdList) external onlyOwner {
+        for (uint256 i = 0; i < _chaindIdList.length; i++) {
+            chainIdExist[_chaindIdList[i]] = true;
+        }
+    }
+
+    function rmChainId(uint8 _chaindId) external onlyOwner {
+        delete chainIdExist[_chaindId];
     }
 
     function setMinAmount(uint256 _minAmount) external onlyOwner {
@@ -36,24 +72,37 @@ contract StakePortal is Ownable {
         relayFee = _relayFee;
     }
 
-    function stake(uint256 amount, bytes memory _data) public payable {
-        require(amount >= minAmount, "amount < minAmount");
-        require(msg.value >= relayFee, "relay fee not enough");
+    function withdrawFee() external onlyOwner {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "failed to withdraw");
+    }
 
-        uint256 balBefore = IERC20(erc20TokenAddress).balanceOf(
-            stakePoolAddress
+    function stake(
+        address _stakePoolAddress,
+        uint256 _amount,
+        bytes memory _recipient,
+        uint8 _destChainId
+    ) public payable {
+        require(_amount >= minAmount, "amount < minAmount");
+        require(msg.value >= relayFee, "relay fee not enough");
+        require(
+            stakePoolAddressExist[_stakePoolAddress],
+            "stake pool not exist"
         );
+        require(chainIdExist[_destChainId], "dest chain id not exit");
+
         IERC20(erc20TokenAddress).safeTransferFrom(
             msg.sender,
-            stakePoolAddress,
-            amount
-        );
-        uint256 balAfter = IERC20(erc20TokenAddress).balanceOf(
-            stakePoolAddress
+            _stakePoolAddress,
+            _amount
         );
 
-        require(balBefore.add(amount) == balAfter, "amount not match");
-
-        emit Stake(msg.sender, amount, _data);
+        emit Stake(
+            msg.sender,
+            _stakePoolAddress,
+            _amount,
+            _recipient,
+            _destChainId
+        );
     }
 }
