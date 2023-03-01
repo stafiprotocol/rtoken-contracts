195045b45b1dfefbb6f5761c5e2492d12a3a8d30
tpkeeper <tpkeeper.me@gmail.com>
2023-03-01 21:36:04 +0800

    clean

--- contracts/multisig/MultisigProxy.sol
+++ contracts/multisig/MultisigProxy.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.7.6;
 
-import "../proxy/TransparentUpgradeableProxy.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
 
 contract MultisigProxy is TransparentUpgradeableProxy {
-    constructor(address _proxyTo, address admin_, bytes memory _data) TransparentUpgradeableProxy(_proxyTo, admin_, _data) {}
+    constructor(
+        address _proxyTo,
+        address admin_,
+        bytes memory _data
+    ) TransparentUpgradeableProxy(_proxyTo, admin_, _data) {}
 }
