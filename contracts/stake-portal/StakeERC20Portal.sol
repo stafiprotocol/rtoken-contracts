e99f9976ff21f71b2f28a2a4123ce99a0acbd4ed
tpkeeper <tpkeeper.me@gmail.com>
2023-03-22 08:57:01 +0800

    new stake portal

--- contracts/stake-portal/StakeERC20Portal.sol
+++ contracts/stake-portal/StakeERC20Portal.sol
@@ -2,13 +2,35 @@ pragma solidity 0.7.6;
 // SPDX-License-Identifier: GPL-3.0-only
 
 import {SafeERC20, IERC20, SafeMath} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "../balancer-metastable-rate-providers/interfaces/IRateProvider.sol";
+import "./Multisig.sol";
 
-contract StakeERC20Portal {
+contract StakeERC20Portal is Multisig, IRateProvider {
+    using SafeCast for *;
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    // Events
-    event Stake(
+    // ---- storage
+
+    address public erc20TokenAddress;
+    address public stakeUsePoolAddress;
+    address public rTokenAddress;
+    uint256 public minStakeAmount;
+    uint256 public relayFee;
+    uint256 public protocolFeeCommission; // decimals 18
+    bool public stakeSwitch;
+    bool public stakeCrossSwitch;
+    uint256 rate; // decimals 18
+
+    mapping(address => bool) public stakePoolAddressExist;
+    mapping(uint8 => bool) public chainIdExist;
+    mapping(uint8 => uint256) public bridgeFee;
+
+    // events
+    event Stake(address staker, address stakePool, uint256 tokenAmount, uint256 rtokenAmount);
+    event Unstake(address staker, address stakePool, uint256 tokenAmount, uint256 rtokenAmount, uint256 burnAmount);
+    event StakeAndCross(
         address staker,
         address stakePool,
         uint256 amount,
@@ -18,28 +40,19 @@ contract StakeERC20Portal {
     );
     event RecoverStake(bytes32 txHash, bytes32 stafiRecipient);
 
-    address public erc20TokenAddress;
-    uint256 public minAmount;
-    uint256 public relayFee;
-    address public owner;
-    bool public stakeSwitch;
-
-    mapping(address => bool) public stakePoolAddressExist;
-    mapping(uint8 => bool) public chainIdExist;
-    mapping(uint8 => uint256) public bridgeFee;
-
-    modifier onlyOwner() {
-        require(owner == msg.sender, "caller is not the owner");
-        _;
-    }
-
     constructor(
         address[] memory _stakePoolAddressList,
+        address[] memory _initialSubAccounts,
         uint8[] memory _chainIdList,
         address _erc20TokenAddress,
-        uint256 _minAmount,
-        uint256 _relayFee
-    ) {
+        address _rTokenAddress,
+        address _stakeUsePoolAddress,
+        uint256 _minStakeAmount,
+        uint256 _relayFee,
+        uint256 _protocolFeeCommission,
+        uint256 _rate,
+        uint256 _initialThreshold
+    ) Multisig(_initialSubAccounts, _initialThreshold) {
         for (uint256 i = 0; i < _stakePoolAddressList.length; i++) {
             stakePoolAddressExist[_stakePoolAddressList[i]] = true;
         }
@@ -47,23 +60,31 @@ contract StakeERC20Portal {
         for (uint256 i = 0; i < _chainIdList.length; i++) {
             chainIdExist[_chainIdList[i]] = true;
         }
+        require(stakePoolAddressExist[_stakeUsePoolAddress], "stake pool not exist");
+        require(_rate > 0, "rate zero");
 
         erc20TokenAddress = _erc20TokenAddress;
-        minAmount = _minAmount;
+        rTokenAddress = _rTokenAddress;
+        minStakeAmount = _minStakeAmount;
+        stakeUsePoolAddress = _stakeUsePoolAddress;
         relayFee = _relayFee;
-        owner = msg.sender;
-        stakeSwitch = true;
+        protocolFeeCommission = _protocolFeeCommission;
+        rate = _rate;
+        stakeSwitch = false;
+        stakeCrossSwitch = true;
     }
 
-    function transferOwnership(address _newOwner) public onlyOwner {
-        require(_newOwner != address(0), "new owner is the zero address");
-        owner = _newOwner;
-    }
+    // ------ settings
 
     function addStakePool(address[] memory _stakePoolAddressList) external onlyOwner {
         for (uint256 i = 0; i < _stakePoolAddressList.length; i++) {
             stakePoolAddressExist[_stakePoolAddressList[i]] = true;
         }
+    }
+
+    function setStakeUsePool(address _stakeUsePoolAddress) external onlyOwner {
+        require(stakePoolAddressExist[_stakeUsePoolAddress], "stake pool not exist");
+        stakeUsePoolAddress = _stakeUsePoolAddress;
     }
 
     function rmStakePool(address _stakePoolAddress) external onlyOwner {
@@ -80,12 +101,21 @@ contract StakeERC20Portal {
         delete chainIdExist[_chaindId];
     }
 
-    function setMinAmount(uint256 _minAmount) external onlyOwner {
-        minAmount = _minAmount;
+    function setMinStakeAmount(uint256 _minStakeAmount) external onlyOwner {
+        minStakeAmount = _minStakeAmount;
     }
 
     function setRelayFee(uint256 _relayFee) external onlyOwner {
         relayFee = _relayFee;
+    }
+
+    function setRate(uint256 _rate) external onlyOwner {
+        require(_rate > 0, "rate zero");
+        rate = _rate;
+    }
+
+    function setProtocolFeeCommission(uint256 _protocolFeeCommission) external onlyOwner {
+        protocolFeeCommission = _protocolFeeCommission;
     }
 
     function setBridgeFee(uint8 _chainId, uint256 _bridgeFee) external onlyOwner {
@@ -93,32 +123,119 @@ contract StakeERC20Portal {
         bridgeFee[_chainId] = _bridgeFee;
     }
 
-    function toggleSwitch() external onlyOwner {
+    function toggleStakeSwitch() external onlyOwner {
         stakeSwitch = !stakeSwitch;
     }
 
-    function withdrawFee() external onlyOwner {
-        (bool success, ) = msg.sender.call{value: address(this).balance}("");
-        require(success, "failed to withdraw");
+    function toggleStakeCrossSwitch() external onlyOwner {
+        stakeCrossSwitch = !stakeCrossSwitch;
     }
 
-    function stake(
+    // ----- getters
+
+    function getRate() external view override returns (uint256) {
+        return rate;
+    }
+
+    // ----- vote
+
+    function voteRate(bytes32 _proposalId, uint256 _rate) public onlySubAccount {
+        require(rate > 0, "rate zero");
+        Proposal memory proposal = proposals[_proposalId];
+
+        require(uint256(proposal._status) <= 1, "proposal already executed");
+        require(!_hasVoted(proposal, msg.sender), "already voted");
+
+        if (proposal._status == ProposalStatus.Inactive) {
+            proposal = Proposal({_status: ProposalStatus.Active, _yesVotes: 0, _yesVotesTotal: 0});
+        }
+        proposal._yesVotes = (proposal._yesVotes | subAccountBit(msg.sender)).toUint16();
+        proposal._yesVotesTotal++;
+
+        // Finalize if Threshold has been reached
+        if (proposal._yesVotesTotal >= threshold) {
+            rate = _rate;
+
+            proposal._status = ProposalStatus.Executed;
+            emit ProposalExecuted(_proposalId);
+        }
+        proposals[_proposalId] = proposal;
+    }
+
+    // ----- staker operation
+
+    function stake(uint256 _amount) public payable {
+        stakeWithPool(_amount, stakeUsePoolAddress);
+    }
+
+    function unstake(uint256 _rTokenAmount) public payable {
+        unstakeWithPool(_rTokenAmount, stakeUsePoolAddress);
+    }
+
+    function stakeWithPool(uint256 _amount, address _stakePoolAddress) public payable {
+        require(stakeSwitch, "stake not open");
+        require(_amount >= minStakeAmount, "amount < minStakeAmount");
+        require(msg.value >= relayFee, "fee not enough");
+        require(stakePoolAddressExist[_stakePoolAddress], "stake pool not exist");
+
+        uint256 rTokenAmount = _amount.mul(1e18).div(rate);
+
+        // transfer token and mint rtoken
+        IERC20(erc20TokenAddress).safeTransferFrom(msg.sender, _stakePoolAddress, _amount);
+        ERC20PresetMinterPauser rToken = ERC20PresetMinterPauser(rTokenAddress);
+        rToken.mint(msg.sender, rTokenAmount);
+
+        // fee
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success, "failed to send fee");
+
+        emit Stake(msg.sender, _stakePoolAddress, _amount, rTokenAmount);
+    }
+
+    function unstakeWithPool(uint256 _rTokenAmount, address _stakePoolAddress) public payable {
+        require(stakeSwitch, "stake not open");
+        require(_rTokenAmount >= 0, "amount zero");
+        require(msg.value >= relayFee, "relay fee not enough");
+        require(stakePoolAddressExist[_stakePoolAddress], "stake pool not exist");
+
+        uint256 protocolFee = _rTokenAmount.mul(protocolFeeCommission).div(1e18);
+        uint256 leftRTokenAmount = _rTokenAmount.sub(protocolFee);
+        uint256 tokenAmount = leftRTokenAmount.mul(rate).div(1e18);
+
+        // burn rtoken
+        ERC20PresetMinterPauser rtoken = ERC20PresetMinterPauser(rTokenAddress);
+        rtoken.burnFrom(msg.sender, leftRTokenAmount);
+
+        // fee
+        IERC20(rTokenAddress).safeTransferFrom(msg.sender, owner, protocolFee);
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success, "failed to send fee");
+
+        emit Unstake(msg.sender, _stakePoolAddress, tokenAmount, _rTokenAmount, leftRTokenAmount);
+    }
+
+    function stakeAndCross(
         address _stakePoolAddress,
         uint256 _amount,
         uint8 _destChainId,
         bytes32 _stafiRecipient,
         address _destRecipient
     ) public payable {
-        require(stakeSwitch, "stake not open");
+        require(stakeCrossSwitch, "stake cross not open");
         require(chainIdExist[_destChainId], "dest chain id not exit");
-        require(_amount >= minAmount, "amount < minAmount");
+        require(_amount >= minStakeAmount, "amount < minStakeAmount");
         require(msg.value >= relayFee.add(bridgeFee[_destChainId]), "fee not enough");
         require(stakePoolAddressExist[_stakePoolAddress], "stake pool not exist");
         require(_stafiRecipient != bytes32(0) && _destRecipient != address(0), "wrong recipient");
 
+        // tranfer rtoken
         IERC20(erc20TokenAddress).safeTransferFrom(msg.sender, _stakePoolAddress, _amount);
 
-        emit Stake(msg.sender, _stakePoolAddress, _amount, _destChainId, _stafiRecipient, _destRecipient);
+        // fee
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success, "failed to send fee");
+
+        emit StakeAndCross(msg.sender, _stakePoolAddress, _amount, _destChainId, _stafiRecipient, _destRecipient);
     }
 
     function recoverStake(bytes32 _txHash, bytes32 _stafiRecipient) public {
