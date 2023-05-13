352b8674c786d78fdc59c6e3ea0eb76eea7db506
tpkeeper <tpkeeper.me@gmail.com>
2023-05-13 13:24:47 +0800

    optimize relayer fee

--- contracts/rbnb/StakeManager.sol
+++ contracts/rbnb/StakeManager.sol
@@ -17,20 +17,23 @@ contract StakeManager is Multisig, IRateProvider {
 
     // ---- storage
 
+    bool public stakeSwitch;
+
+    // params
     address public rTokenAddress;
     uint256 public minStakeAmount;
     uint256 public unstakeFeeCommission; // decimals 18
     uint256 public protocolFeeCommission; // decimals 18
     uint256 public rateChangeLimit; // decimals 18
-    bool public stakeSwitch;
-
-    uint256 public currentEra;
-    uint256 private rate; //need migrate decimals 18
-    uint256 public totalRTokenSupply; //need migrate
-    uint256 public totalProtocolFee; //need migrate
-
+    uint256 public transferGas;
     uint256 public eraSeconds;
-    uint256 nextUnstakeIndex;
+
+    // era state
+    uint256 public currentEra; // need migrate
+    uint256 private rate; // need migrate decimals 18
+    uint256 public totalRTokenSupply; // need migrate
+    uint256 public totalProtocolFee; // need migrate
+
     EnumerableSet.AddressSet bondedPools;
     mapping(address => PoolInfo) poolInfoOf;
     mapping(address => uint256) latestRewardTimestampOf;
@@ -39,6 +42,8 @@ contract StakeManager is Multisig, IRateProvider {
     mapping(address => uint256) pendingUndelegateOf;
     mapping(uint256 => mapping(address => Operate)) pendingOperate;
 
+    // unstake info
+    uint256 nextUnstakeIndex;
     mapping(uint256 => UnstakeInfo) unstakeAtIndex;
     mapping(address => EnumerableSet.UintSet) unstakeOfUser;
 
@@ -53,14 +58,19 @@ contract StakeManager is Multisig, IRateProvider {
         uint256 _minStakeAmount,
         uint256 _unstakeFeeCommission,
         address _stakePoolAddress,
-        uint256 _bond, //need migrate
-        uint256 _unbond, //need migrate
-        uint256 _active, //need migrate
-        uint256 _rate, //need migrate
-        uint256 _totalRTokenSupply, //need migrate
-        uint256 _totalProtocolFee //need migrate
+        uint256 _bond, // need migrate
+        uint256 _unbond, // need migrate
+        uint256 _active, // need migrate
+        uint256 _rate, // need migrate
+        uint256 _totalRTokenSupply, // need migrate
+        uint256 _totalProtocolFee, // need migrate
+        uint256 _era // need migrate
     ) public {
         initMultisig(_initialSubAccounts, _initialThreshold);
+
+        rTokenAddress = _rTokenAddress;
+        minStakeAmount = _minStakeAmount;
+        unstakeFeeCommission = _unstakeFeeCommission;
 
         bondedPools.add(_stakePoolAddress);
         poolInfoOf[_stakePoolAddress] = PoolInfo({
@@ -69,17 +79,14 @@ contract StakeManager is Multisig, IRateProvider {
             unbond: _unbond,
             active: _active
         });
-
-        require(_rate > 0, "rate zero");
         rate = _rate;
-
-        rTokenAddress = _rTokenAddress;
-        minStakeAmount = _minStakeAmount;
-        unstakeFeeCommission = _unstakeFeeCommission;
-        rateChangeLimit = 1e15; // 0.1%
-        protocolFeeCommission = 1e17;
         totalRTokenSupply = _totalRTokenSupply;
         totalProtocolFee = _totalProtocolFee;
+        currentEra = _era;
+
+        rateChangeLimit = 1e15; // 0.1%
+        protocolFeeCommission = 1e17;
+        transferGas = 2300;
     }
 
     // ------ settings
@@ -96,12 +103,16 @@ contract StakeManager is Multisig, IRateProvider {
         uint256 _unstakeFeeCommission,
         uint256 _protocolFeeCommission,
         uint256 _minStakeAmount,
-        uint256 _rateChangeLimit
+        uint256 _rateChangeLimit,
+        uint256 _eraSeconds,
+        uint256 _transferGas
     ) external onlyOwner {
         unstakeFeeCommission = _unstakeFeeCommission;
         protocolFeeCommission = _protocolFeeCommission;
         minStakeAmount = _minStakeAmount;
         rateChangeLimit = _rateChangeLimit;
+        eraSeconds = _eraSeconds;
+        transferGas = _transferGas;
     }
 
     function toggleStakeSwitch() external onlyOwner {
@@ -135,15 +146,15 @@ contract StakeManager is Multisig, IRateProvider {
         address[] calldata _poolAddressList,
         uint256[] calldata _undistributedRewardList,
         uint256[] calldata _latestRewardTimestampList
-    ) public onlySubAccount {
+    ) external onlySubAccount {
         bytes32 proposalId = keccak256(
             abi.encodePacked("newEra", _era, _poolAddressList, _undistributedRewardList, _latestRewardTimestampList)
         );
-        Proposal memory proposal = checkProposal(proposalId);
+        Proposal memory proposal = _checkProposal(proposalId);
 
         // Finalize if Threshold has been reached
         if (proposal._yesVotesTotal >= threshold) {
-            exeNewEra(_era, _poolAddressList, _undistributedRewardList, _latestRewardTimestampList);
+            _exeNewEra(_era, _poolAddressList, _undistributedRewardList, _latestRewardTimestampList);
 
             proposal._status = ProposalStatus.Executed;
             emit ProposalExecuted(proposalId);
@@ -158,13 +169,13 @@ contract StakeManager is Multisig, IRateProvider {
         Action _action,
         address[] calldata _valList,
         uint256[] calldata _amountList
-    ) public onlySubAccount {
+    ) external onlySubAccount {
         bytes32 proposalId = keccak256(abi.encodePacked("operate", _era, _poolAddress, _action, _valList, _amountList));
-        Proposal memory proposal = checkProposal(proposalId);
+        Proposal memory proposal = _checkProposal(proposalId);
 
         // Finalize if Threshold has been reached
         if (proposal._yesVotesTotal >= threshold) {
-            exeOperate(_era, _poolAddress, _action, _valList, _amountList);
+            _exeOperate(_era, _poolAddress, _action, _valList, _amountList);
 
             proposal._status = ProposalStatus.Executed;
             emit ProposalExecuted(proposalId);
@@ -177,13 +188,13 @@ contract StakeManager is Multisig, IRateProvider {
         uint256 _era,
         address _poolAddress,
         uint256[] calldata _successOpIndexList
-    ) public onlySubAccount {
+    ) external onlySubAccount {
         bytes32 proposalId = keccak256(abi.encodePacked("operateAck", _era, _poolAddress, _successOpIndexList));
-        Proposal memory proposal = checkProposal(proposalId);
+        Proposal memory proposal = _checkProposal(proposalId);
 
         // Finalize if Threshold has been reached
         if (proposal._yesVotesTotal >= threshold) {
-            exeOperateAck(_era, _poolAddress, _successOpIndexList);
+            _exeOperateAck(_era, _poolAddress, _successOpIndexList);
 
             proposal._status = ProposalStatus.Executed;
             emit ProposalExecuted(proposalId);
@@ -194,10 +205,12 @@ contract StakeManager is Multisig, IRateProvider {
 
     // ----- staker operation
 
-    function stake(address _stakePoolAddress) public payable {
+    function stake(address _stakePoolAddress) external payable {
         require(stakeSwitch, "stake closed");
         require(msg.value >= minStakeAmount, "amount not match");
         require(bondedPools.contains(_stakePoolAddress), "pool not exist");
+        (bool success, ) = msg.sender.call{gas: transferGas}("");
+        require(success, "staker not payable");
 
         uint256 rTokenAmount = msg.value.mul(1e18).div(rate);
 
@@ -207,7 +220,7 @@ contract StakeManager is Multisig, IRateProvider {
         poolInfo.active = poolInfo.active.add(msg.value);
 
         // transfer token
-        (bool success, ) = _stakePoolAddress.call{value: msg.value}("");
+        (success, ) = _stakePoolAddress.call{value: msg.value}("");
         require(success, "transfer failed");
 
         // mint rtoken
@@ -217,10 +230,12 @@ contract StakeManager is Multisig, IRateProvider {
         emit Stake(msg.sender, _stakePoolAddress, msg.value, rTokenAmount);
     }
 
-    function unstake(uint256 _rTokenAmount, address _stakePoolAddress) public payable {
+    function unstake(uint256 _rTokenAmount, address _stakePoolAddress) external {
         require(stakeSwitch, "stake closed");
         require(_rTokenAmount >= 0, "amount zero");
         require(bondedPools.contains(_stakePoolAddress), "pool not exist");
+        (bool success, ) = msg.sender.call{gas: transferGas}("");
+        require(success, "unstaker not payable");
 
         uint256 unstakeFee = _rTokenAmount.mul(unstakeFeeCommission).div(1e18);
         uint256 leftRTokenAmount = _rTokenAmount.sub(unstakeFee);
@@ -239,8 +254,8 @@ contract StakeManager is Multisig, IRateProvider {
         totalProtocolFee = totalProtocolFee.add(unstakeFee);
         IERC20(rTokenAddress).safeTransferFrom(msg.sender, owner, unstakeFee);
 
+        // unstake info
         unstakeAtIndex[nextUnstakeIndex] = UnstakeInfo({era: currentEra, receiver: msg.sender, amount: tokenAmount});
-
         unstakeOfUser[msg.sender].add(nextUnstakeIndex);
 
         nextUnstakeIndex = nextUnstakeIndex.add(1);
@@ -248,7 +263,7 @@ contract StakeManager is Multisig, IRateProvider {
         emit Unstake(msg.sender, _stakePoolAddress, tokenAmount, _rTokenAmount, leftRTokenAmount);
     }
 
-    function claim(uint256[] calldata _unstakeIndexList) public {
+    function claim(uint256[] calldata _unstakeIndexList) external {
         require(_unstakeIndexList.length > 0, "index empty");
 
         uint256 totalAmount;
@@ -270,7 +285,7 @@ contract StakeManager is Multisig, IRateProvider {
 
     // ----- helper
 
-    function checkProposal(bytes32 _proposalId) private view returns (Proposal memory proposal) {
+    function _checkProposal(bytes32 _proposalId) private view returns (Proposal memory proposal) {
         proposal = proposals[_proposalId];
 
         require(uint256(proposal._status) <= 1, "proposal already executed");
@@ -283,7 +298,7 @@ contract StakeManager is Multisig, IRateProvider {
         proposal._yesVotesTotal++;
     }
 
-    function exeNewEra(
+    function _exeNewEra(
         uint256 _era,
         address[] calldata _poolAddressList,
         uint256[] calldata _undistributedRewardList,
@@ -327,8 +342,8 @@ contract StakeManager is Multisig, IRateProvider {
             uint256 pendingUndelegate = pendingUndelegateOf[poolAddress].add(poolInfo.unbond);
 
             uint256 diff = pendingDelegate > pendingUndelegate
-                ? pendingDelegate - pendingUndelegate
-                : pendingUndelegate - pendingDelegate;
+                ? pendingDelegate.sub(pendingUndelegate)
+                : pendingUndelegate.sub(pendingDelegate);
 
             pendingDelegateOf[poolAddress] = pendingDelegate.sub(diff);
             pendingUndelegateOf[poolAddress] = pendingUndelegate.sub(diff);
@@ -366,11 +381,12 @@ contract StakeManager is Multisig, IRateProvider {
         uint256 newRate = totalNewActive.mul(1e18).div(totalRTokenSupply);
         uint256 rateChange = newRate > rate ? newRate.sub(rate) : rate.sub(newRate);
         require(rateChange.mul(1e18).div(rate) < rateChangeLimit, "rate change over limit");
+
         // update era
         currentEra = _era;
     }
 
-    function exeOperate(
+    function _exeOperate(
         uint256 _era,
         address _poolAddress,
         Action _action,
@@ -391,9 +407,9 @@ contract StakeManager is Multisig, IRateProvider {
         poolInfoOf[_poolAddress].eraState = EraState.OperateExecuted;
     }
 
-    function exeOperateAck(uint256 _era, address _poolAddress, uint256[] calldata _successOpIndexList) private {
+    function _exeOperateAck(uint256 _era, address _poolAddress, uint256[] calldata _successOpIndexList) private {
         require(currentEra == _era, "era not match");
-        require(poolInfoOf[_poolAddress].eraState == EraState.OperateExecuted, "erState not match");
+        require(poolInfoOf[_poolAddress].eraState == EraState.OperateExecuted, "eraState not match");
 
         Operate memory op = pendingOperate[_era][_poolAddress];
         if (op.action == Action.Delegate) {
--- contracts/rbnb/StakePool.sol
+++ contracts/rbnb/StakePool.sol
@@ -1,55 +1,90 @@
 pragma solidity 0.7.6;
 import "./IStaking.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 
 // SPDX-License-Identifier: GPL-3.0-only
 contract StakePool {
-    address public stakingAddress;
-    address public stakeManager;
+    using SafeMath for uint256;
+
+    bytes32 private constant stakingAddressSlot = bytes32(uint256(keccak256("StakePool.proxy.stakingAddressSlot")) - 1);
+    bytes32 private constant stakeManagerAddressSlot =
+        bytes32(uint256(keccak256("StakePool.proxy.stakeManagerAddressSlot")) - 1);
 
     modifier onlyStakeManager() {
-        require(msg.sender == stakeManager, "only stakeManager");
+        require(msg.sender == stakeManagerAddress(), "only stakeManager");
         _;
     }
 
-    function init(address _stakingAddress, address _stakeManger) external {
-        require(stakingAddress == address(0), "already init");
-        stakingAddress = _stakingAddress;
-        stakeManager = _stakeManger;
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    function init(address _stakingAddress, address _stakeMangerAddress) external {
+        require(stakingAddress() == address(0), "already init");
+        setStakingAddress(_stakingAddress);
+        setStakeManagerAddress(_stakeMangerAddress);
+    }
+
+    function stakingAddress() public view returns (address impl) {
+        bytes32 slot = stakingAddressSlot;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            impl := sload(slot)
+        }
+    }
+
+    function stakeManagerAddress() public view returns (address impl) {
+        bytes32 slot = stakeManagerAddressSlot;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            impl := sload(slot)
+        }
+    }
+
+    function setStakingAddress(address _stakingAddress) private {
+        bytes32 slot = stakingAddressSlot;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            sstore(slot, _stakingAddress)
+        }
+    }
+
+    function setStakeManagerAddress(address _stakeManagerAddress) private {
+        bytes32 slot = stakeManagerAddressSlot;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            sstore(slot, _stakeManagerAddress)
+        }
     }
 
     function checkAndClaimReward() external onlyStakeManager returns (uint256) {
-        if (IStaking(stakingAddress).getDistributedReward(address(this)) > 0) {
-            return IStaking(stakingAddress).claimReward();
+        if (IStaking(stakingAddress()).getDistributedReward(address(this)) > 0) {
+            return IStaking(stakingAddress()).claimReward();
         }
         return 0;
     }
 
     function checkAndClaimUndelegated() external onlyStakeManager returns (uint256) {
-        if (IStaking(stakingAddress).getUndelegated(address(this)) > 0) {
-            return IStaking(stakingAddress).claimUndelegated();
+        if (IStaking(stakingAddress()).getUndelegated(address(this)) > 0) {
+            return IStaking(stakingAddress()).claimUndelegated();
         }
         return 0;
     }
 
-    function delegate(
-        address[] calldata validatorList,
-        uint256[] calldata amountList
-    ) external payable onlyStakeManager {
+    function delegate(address[] calldata validatorList, uint256[] calldata amountList) external onlyStakeManager {
+        uint256 relayerFee = IStaking(stakingAddress()).getRelayerFee();
         for (uint256 i = 0; i < validatorList.length; ++i) {
-            IStaking(stakingAddress).delegate(validatorList[i], amountList[i]);
+            IStaking(stakingAddress()).delegate{value: amountList[i].add(relayerFee)}(validatorList[i], amountList[i]);
         }
     }
 
-    function undelegate(
-        address[] calldata validatorList,
-        uint256[] calldata amountList
-    ) external payable onlyStakeManager {
+    function undelegate(address[] calldata validatorList, uint256[] calldata amountList) external onlyStakeManager {
+        uint256 relayerFee = IStaking(stakingAddress()).getRelayerFee();
         for (uint256 i = 0; i < validatorList.length; ++i) {
-            IStaking(stakingAddress).undelegate(validatorList[i], amountList[i]);
+            IStaking(stakingAddress()).undelegate{value: relayerFee}(validatorList[i], amountList[i]);
         }
     }
 
     function getTotalDelegated() external view returns (uint256) {
-        return IStaking(stakingAddress).getTotalDelegated(address(this));
+        return IStaking(stakingAddress()).getTotalDelegated(address(this));
     }
 }
