e1397f655d6b13e6059c8bf5e8318e0202e6a8a5
tpkeeper <tpkeeper.me@gmail.com>
2023-06-20 10:50:06 +0800

    update networks

--- README.md
+++ README.md
@@ -20,19 +20,3 @@ yarn compile
 ```bash
 yarn  test
 ```
-
-### deploy
-
-Preparation:
-- create `.env` file if it does not exist
-- Set `MNEMONIC` in `.env`
-- Set `INFURA_KEY` in `.env`
-- Set `ETHERSCAN_API_KEY` in `.env`
-
-```bash
-yarn hardhat --network <network> deploy
-```
-- network can be: mainnet, xdai, ewc, rinkeby, goerli, kovan, volta. add network to hardhat.config.ts if you'd like to support new network.
-
-
-
--- hardhat.config.js
+++ hardhat.config.js
@@ -24,20 +24,12 @@ module.exports = {
     },
     local: {
       url: 'http://127.0.0.1:8545',
-      accounts: [
-        '7c12708ee2f423e86d3b77e6bef12907ca61b2d97a7195f93c1137dc08098b74', // superuser
-        '13d28b99efc64e66d4bb549f2dba1324305d47629b2141f1c16163f6ff6a2206', // trustnode
-        '45a690ae0fab855d60f592b2cd561a730a1d3ab60e138c5366b9e27778c066d9', // supernode
-        '9e078940152584a642aaed17ebb97509274f874e176695de947ade3a1d8fe353', // 
-        'b7d6bef9fec45408c5d6c7f3182ab409f0774549481179ee627e87b8ec43980f', // 
-        'cf9021015de4fe7559b981e098f46659b7f9a9dc28e89b7f554936c7aadaf822', // 
-      ],
     },
     ethMainnet: {
-      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://rpc.ankr.com/eth`,
     },
     goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      url: `https://rpc.ankr.com/eth_goerli`,
     },
     bscmainnet: {
       url: `https://rpc.ankr.com/bsc`,
@@ -47,6 +39,9 @@ module.exports = {
     },
     polygonMainnet: {
       url: `https://rpc.ankr.com/polygon`,
+    },
+    arbitrumMainnet: {
+      url: `https://rpc.ankr.com/arbitrum`,
     }
   },
   defaultNetwork: "hardhat",
